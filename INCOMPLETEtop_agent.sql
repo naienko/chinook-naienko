8d044cc08bab2b89f24a986a859f3cf2b3f647f9
Panya Farnette <naienko@gmail.com>
2019-05-20 13:34:39 -0500

    cleaned up code

--- INCOMPLETEtop_agent.sql
+++ INCOMPLETEtop_agent.sql
@@ -1,8 +1,8 @@
 --Which sales agent made the most in sales over all?
 
 SELECT TOP 1
-    MAX(x.SalesAgentTotal),
-    x.FullName
+    MAX(Sales.SalesAgentTotal) AS Total,
+    Sales.FullName
 FROM
 (SELECT SUM(i.Total) AS SalesAgentTotal,
     e.FirstName + ' ' + e.LastName AS FullName,
@@ -10,6 +10,6 @@ FROM
 FROM Invoice i
 JOIN Customer c ON i.CustomerId = c.CustomerId
 JOIN Employee e ON c.SupportRepId = e.EmployeeId
-GROUP BY e.LastName, e.FirstName, e.EmployeeId) x
-GROUP BY x.FullName, x.SalesAgentTotal
-ORDER BY x.SalesAgentTotal DESC;
+GROUP BY e.LastName, e.FirstName, e.EmployeeId) Sales
+GROUP BY Sales.FullName, Sales.SalesAgentTotal
+ORDER BY Total DESC;
--- INCOMPLETEtop_country.sql
+++ INCOMPLETEtop_country.sql
@@ -1,19 +1,12 @@
 --Which country's customers spent the most?
 
 SELECT TOP 1
-MAX(x.CountryTotal),
-    x.BillingCountry
+MAX(CountrySales.CountryTotal) AS Total,
+    CountrySales.BillingCountry AS Country
 FROM
 (SELECT SUM(i.Total) AS CountryTotal,
     i.BillingCountry
 FROM Invoice i
-GROUP BY i.BillingCountry) x
-GROUP BY x.BillingCountry, x.CountryTotal
-ORDER BY x.CountryTotal DESC;
-
-SELECT x.BillingCountry
-FROM (SELECT SUM(i.Total) AS CountryTotal,
-        i.BillingCountry
-    FROM Invoice i
-    GROUP BY i.BillingCountry) x
-WHERE MAX(x.CountryTotal) = MAX (x.CountryTotal);
+GROUP BY i.BillingCountry) CountrySales
+GROUP BY CountrySales.BillingCountry, CountrySales.CountryTotal
+ORDER BY CountrySales.CountryTotal DESC;
